4c02da0d4cad44414651f6f72eb5de0150ee710c
Robin Baggett <hellorobinbaggett@gmail.com>
2025-07-12 14:47:50 -0500

    push

--- scripts/manager_action_skill.lua
+++ scripts/manager_action_skill.lua
@@ -190,11 +190,11 @@ function modRoll(rSource, rTarget, rRoll)
 
 		-- Get roll effect modifiers
 		local nEffectCount;
-		aAddDice, nAddMod, nEffectCount = EffectManager5E.getEffectsBonus(rSource, {"CHECK"}, false, aCheckFilter);
+		aAddDice, nAddMod, nEffectCount = EffectManager_DS.getEffectsBonus(rSource, {"CHECK"}, false, aCheckFilter);
 		if (nEffectCount > 0) then
 			bEffects = true;
 		end
-		local aSkillAddDice, nSkillAddMod, nSkillEffectCount = EffectManager5E.getEffectsBonus(rSource, {"SKILL"}, false, aSkillFilter);
+		local aSkillAddDice, nSkillAddMod, nSkillEffectCount = EffectManager_DS.getEffectsBonus(rSource, {"SKILL"}, false, aSkillFilter);
 		if (nSkillEffectCount > 0) then
 			bEffects = true;
 			for _,v in ipairs(aSkillAddDice) do
@@ -204,46 +204,46 @@ function modRoll(rSource, rTarget, rRoll)
 		end
 		
 		-- Get condition modifiers
-		if EffectManager5E.hasEffectCondition(rSource, "ADVSKILL") then
+		if EffectManager_DS.hasEffectCondition(rSource, "ADVSKILL") then
 			bADV = true;
 			bEffects = true;
-		elseif #(EffectManager5E.getEffectsByType(rSource, "ADVSKILL", aSkillFilter)) > 0 then
+		elseif #(EffectManager_DS.getEffectsByType(rSource, "ADVSKILL", aSkillFilter)) > 0 then
 			bADV = true;
 			bEffects = true;
-		elseif EffectManager5E.hasEffectCondition(rSource, "ADVCHK") then
+		elseif EffectManager_DS.hasEffectCondition(rSource, "ADVCHK") then
 			bADV = true;
 			bEffects = true;
-		elseif #(EffectManager5E.getEffectsByType(rSource, "ADVCHK", aCheckFilter)) > 0 then
+		elseif #(EffectManager_DS.getEffectsByType(rSource, "ADVCHK", aCheckFilter)) > 0 then
 			bADV = true;
 			bEffects = true;
 		end
-		if EffectManager5E.hasEffectCondition(rSource, "DISSKILL") then
+		if EffectManager_DS.hasEffectCondition(rSource, "DISSKILL") then
 			bDIS = true;
 			bEffects = true;
-		elseif #(EffectManager5E.getEffectsByType(rSource, "DISSKILL", aSkillFilter)) > 0 then
+		elseif #(EffectManager_DS.getEffectsByType(rSource, "DISSKILL", aSkillFilter)) > 0 then
 			bDIS = true;
 			bEffects = true;
-		elseif EffectManager5E.hasEffectCondition(rSource, "DISCHK") then
+		elseif EffectManager_DS.hasEffectCondition(rSource, "DISCHK") then
 			bDIS = true;
 			bEffects = true;
-		elseif #(EffectManager5E.getEffectsByType(rSource, "DISCHK", aCheckFilter)) > 0 then
-			bDIS = true;
-			bEffects = true;
-		end
-		if EffectManager5E.hasEffectCondition(rSource, "Frightened") then
+		elseif #(EffectManager_DS.getEffectsByType(rSource, "DISCHK", aCheckFilter)) > 0 then
 			bDIS = true;
 			bEffects = true;
 		end
-		if EffectManager5E.hasEffectCondition(rSource, "Intoxicated") then
+		if EffectManager_DS.hasEffectCondition(rSource, "Frightened") then
 			bDIS = true;
 			bEffects = true;
 		end
-		if EffectManager5E.hasEffectCondition(rSource, "Poisoned") then
+		if EffectManager_DS.hasEffectCondition(rSource, "Intoxicated") then
+			bDIS = true;
+			bEffects = true;
+		end
+		if EffectManager_DS.hasEffectCondition(rSource, "Poisoned") then
 			bDIS = true;
 			bEffects = true;
 		end
 		if StringManager.contains({ "strength", "dexterity", "constitution" }, sAbility) then
-			if EffectManager5E.hasEffectCondition(rSource, "Encumbered") then
+			if EffectManager_DS.hasEffectCondition(rSource, "Encumbered") then
 				bEffects = true;
 				bDIS = true;
 			end
@@ -257,7 +257,7 @@ function modRoll(rSource, rTarget, rRoll)
 		end
 		
 		-- Get exhaustion modifiers
-		local nExhaustMod, nExhaustCount = EffectManager5E.getEffectsBonus(rSource, {"EXHAUSTION"}, true);
+		local nExhaustMod, nExhaustCount = EffectManager_DS.getEffectsBonus(rSource, {"EXHAUSTION"}, true);
 		if nExhaustCount > 0 then
 			bEffects = true;
 			if nExhaustMod >= 1 then
--- scripts/manager_initiative_ds.lua
+++ scripts/manager_initiative_ds.lua
@@ -13,6 +13,7 @@ function saveRoll(rMessage, rRoll)
 
 	if rRoll.aDice[1].result > 5 then
 		rMessage.text = tostring(rMessage.text) .. " Success! [Effect Ends]";
+		-- remove condition
 	else
 		rMessage.text = tostring(rRoll.sDesc) .. " Failure [Effect Continues]";
 	end
