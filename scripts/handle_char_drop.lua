73e698ab1628452706d65a72b82d7a6100a85b51
Robin Baggett <hellorobinbaggett@gmail.com>
2025-07-17 12:19:42 -0500

    classes (level up), ancestries, kits working

--- scripts/handle_char_drop.lua
+++ scripts/handle_char_drop.lua
@@ -38,27 +38,48 @@ function onDrop(x, y, draginfo)
             -- class - WIP
             if StringManager.contains({ "class" }, sClass) then
                 local heroClass = DB.getChild(node, "classtitle");
-                local heroFeatures = DB.createChild(node, "featureslist");
+                local heroFeaturesList = DB.createChild(node, "featureslist");
                 local heroLevel = DB.getChild(node, "levelnumbertitle");
 
                 local nodeSource = DB.findNode(sRecord);
                 local className = DB.getChild(nodeSource, "name");
-                local classFeatures = DB.createChild(nodeSource, "features");
+                local classFeaturesList = DB.createChild(nodeSource, "features");
 
                 -- add only the class features that are equal to or below class level
                 local tNodes = DB.getChildren(nodeSource, "features")
-                local test = DB.getChildren(node, "featureslist")
-                
                 for key,value in pairs(tNodes) do
                     local level = DB.createChild(value, "level");
                     if(level.getValue() == heroLevel.getValue()) then
-                        -- table.insert(test, value);
-                        -- DB.copyNode(value, heroFeatures);
+                        DB.createChildAndCopy(heroFeaturesList, value);
                     end
                 end
 
-                DB.copyNode(className, heroClass);
-                DB.copyNode(classFeatures, heroFeatures);
+                local staminaPerLevel = DB.getChild(nodeSource, "staminaPerLevel");
+                local startingStamina = DB.getChild(nodeSource, "startingStamina");
+                local heroMaxStamina = DB.getChild(nodeSource, "hp.stamina");
+                local heroCurrentStamina = DB.getChild(nodeSource, "hp.max");
+                if(heroLevel.getValue() == 1) then
+                    DB.copyNode(startingStamina, DB.getChild(node, "hp.stamina"));
+                    DB.copyNode(startingStamina, DB.getChild(node, "hp.max"));
+                end
+                if(heroLevel.getValue() > 1) then
+                    local levelMultiplyer = (heroLevel.getValue() - 1)
+                    local additionalStamina = (staminaPerLevel.getValue() * levelMultiplyer);
+                    local totalStamina = (additionalStamina + startingStamina.getValue());
+                    Debug.chat(totalStamina);
+                    DB.setValue(node, "hp.stamina", "number", totalStamina);
+                    DB.setValue(node, "hp.max", "number", totalStamina);
+                end
+                
+
+                DB.copyNode(DB.getChild(nodeSource, "name"), DB.getChild(node, "classtitle"));
+                DB.copyNode(DB.getChild(nodeSource, "startingRecoveries"), DB.getChild(node, "recoverycurrent"));
+                DB.copyNode(DB.getChild(nodeSource, "startingRecoveries"), DB.getChild(node, "recoverymax"));
+                DB.copyNode(DB.getChild(nodeSource, "startingMight"), DB.getChild(node, "MGT"));
+                DB.copyNode(DB.getChild(nodeSource, "startingAgility"), DB.getChild(node, "AGL"));
+                DB.copyNode(DB.getChild(nodeSource, "startingReason"), DB.getChild(node, "REA"));
+                DB.copyNode(DB.getChild(nodeSource, "startingIntuition"), DB.getChild(node, "INU"));
+                DB.copyNode(DB.getChild(nodeSource, "startingPresence"), DB.getChild(node, "PRS"));
 
                 local heroName = DB.getChild(node, "name").getValue();
                 ChatManager.SystemMessageResource("char_abilities_message_classadd", tostring(className.getValue()), tostring(heroName));
