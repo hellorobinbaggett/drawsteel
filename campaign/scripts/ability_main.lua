759fb44d84ac452e5bd1d300de02f69a6cd27bbd
Robin Baggett <hellorobinbaggett@gmail.com>
2025-07-21 13:57:42 -0500

    Upload to include structured data (db.xml) for xml examples

--- campaign/scripts/ability_main.lua
+++ campaign/scripts/ability_main.lua
@@ -18,27 +18,30 @@ function update()
 	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
 	local bID = LibraryData.getIDState("npc", nodeRecord);
 
-	local bSection3 = false;
-	if WindowManager.callSafeControlUpdate(self, "trigger", bReadOnly) then bSection3 = true; end;
-	local bSection4 = false;
-	if WindowManager.callSafeControlUpdate(self, "effect", bReadOnly) then bSection3 = true; end;
-	local bSection4 = false;
-	if WindowManager.callSafeControlUpdate(self, "special", bReadOnly) then bSection3 = true; end;
-	local bSection5 = false;
-	if WindowManager.callSafeControlUpdate(self, "class", bReadOnly) then bSection5 = true; end;
-	if WindowManager.callSafeControlUpdate(self, "subclass", bReadOnly) then bSection5 = true; end;
-	if WindowManager.callSafeControlUpdate(self, "ancestry", bReadOnly) then bSection5 = true; end;
-	if WindowManager.callSafeControlUpdate(self, "ability_cost", bReadOnly) then bSection5 = true; end;
-	if WindowManager.callSafeControlUpdate(self, "ability_level", bReadOnly) then bSection5 = true; end;
-	if WindowManager.callSafeControlUpdate(self, "abilitytype", bReadOnly) then bSection5 = true; end;
+	-- HIDES empty sections
+	-- local bSection1 = false;
+	-- local bSection2 = false;
+	-- local bSection3 = false;
+	-- local bSection4 = false;
 
-	divider2.setVisible(bSection2);
-	divider3.setVisible(bSection3);
-	divider4.setVisible(bSection4);
-	divider5.setVisible(bSection5);
+	-- if WindowManager.callSafeControlUpdate(self, "trigger", bReadOnly) then bSection3 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "effect", bReadOnly) then bSection3 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "special", bReadOnly) then bSection3 = true; end;
 
-	WindowManager.callSafeControlUpdate(self, "characteristic_label", bReadOnly);
-	WindowManager.callSafeControlUpdate(self, "characteristic_old", bReadOnly);
+	
+	-- if WindowManager.callSafeControlUpdate(self, "class", bReadOnly) then bSection4 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "subclass", bReadOnly) then bSection4 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "ancestry", bReadOnly) then bSection4 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "ability_cost", bReadOnly) then bSection4 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "ability_level", bReadOnly) then bSection4 = true; end;
+	-- if WindowManager.callSafeControlUpdate(self, "abilitytype", bReadOnly) then bSection4 = true; end;
+
+	-- divider1.setVisible(bSection1);
+	-- divider2.setVisible(bSection2);
+	-- divider3.setVisible(bSection3);
+	-- divider4.setVisible(bSection4);
+
+	-- hides fields
 	WindowManager.callSafeControlUpdate(self, "tier1", bReadOnly);
 	WindowManager.callSafeControlUpdate(self, "tier2", bReadOnly);
 	WindowManager.callSafeControlUpdate(self, "tier3", bReadOnly);
@@ -55,6 +58,6 @@ function update()
 	WindowManager.callSafeControlUpdate(self, "ability_cost", bReadOnly);
 	WindowManager.callSafeControlUpdate(self, "ability_level", bReadOnly);
 	WindowManager.callSafeControlUpdate(self, "abilitytype", bReadOnly);
-	
-	notes.setReadOnly(bReadOnly);
+	WindowManager.callSafeControlUpdate(self, "notes", bReadOnly);
+
 end
