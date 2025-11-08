0f2a7d35b5b03ae677abf87dc72fbaca63343fdc
Robin Baggett <hellorobinbaggett@gmail.com>
2025-11-08 09:39:42 -0600

    update recovery logic

--- campaign/scripts/stamina.lua
+++ campaign/scripts/stamina.lua
@@ -4,9 +4,15 @@ function onValueChanged()
     local nMax = DB.getValue(nodeWin, "stamina.max");
     local nWinded = DB.getValue(nodeWin, "hp.winded");
     local nDead = (0 - nWinded);
-    
+    local nRecovery = DB.getValue(nodeWin, "recoverycurrent");
+    local nMaxRecovery = DB.getValue(nodeWin, "recoverymax");
+    -- handle stamina
     if(nCurrent > nMax) then
         setValue(nMax);
+    end
+    -- handle recoveries
+    if(nRecovery > nMaxRecovery) then
+        setValue(nMaxRecovery);
     end
 
     if(nCurrent < nDead) then
